485474ac3a6be784a0ec2a1c2a7de209a6ebf675
Tony S <sanjayatony@gmail.com>
2023-06-28 18:04:04 +0800

    update header

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
 	return (
 		<header className="px-4 max-w-prose mx-auto mb-20">
 			<h1 className="text-4xl font-bold mb-2 font-space">Bapak D. Ev</h1>
-			<p className="text-sm font-jet">Bapak-bapak who will rock the world</p>
+			<p className="text-sm font-jet">Bapak-bapak who will rock the world (wide web)</p>
 		</header>
 	);
 }
