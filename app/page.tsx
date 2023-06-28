a09cddfb43744e4f9a76c8ec2277185c7c3d3f21
Tony S <sanjayatony@gmail.com>
2023-06-28 15:22:40 +0800

    update footer

--- app/page.tsx
+++ app/page.tsx
@@ -12,35 +12,8 @@ export default async function Homepage() {
 			<main className="px-4 prose prose-stone mx-auto home">
 				<section className="p-6 stack-sm border-2 border-black mb-12 bg-pink-200 font-jet prose-p:m-0">
 					<p>
-						I am a father with over 10 years of experience as a web developer, specializing in{" "}
-						<a
-							href="https://wordpress.org"
-							target="_blank"
-						>
-							WordPress
-						</a>
-						,{" "}
-						<a
-							href="https://nextjs.org"
-							target="_blank"
-						>
-							Next.js
-						</a>
-						,{" "}
-						<a
-							href="https://astro.build"
-							target="_blank"
-						>
-							Astro
-						</a>
-						, and{" "}
-						<a
-							href="https://tailwindcss.com"
-							target="_blank"
-						>
-							Tailwind CSS
-						</a>
-						. Since 2015, I have been successfully working remotely, embracing the flexibility and freedom it offers.
+						I am a father with over 10 years of experience as a web developer, specializing in WordPress, Next.js, Astro, and Tailwind CSS. Since 2015, I have
+						been successfully working remotely, embracing the flexibility and freedom it offers.
 					</p>
 				</section>
 				<section>
@@ -79,24 +52,6 @@ export default async function Homepage() {
 						appreciation for family, I bring a balanced perspective to every project. With the blend of remote work advantages, technical expertise, and years
 						of experience, I deliver exceptional web solutions that seamlessly merge functionality and aesthetic appeal.
 					</p>
-				</section>
-
-				<section>
-					<svg
-						className="w-64 h-64 transform rotate-[35deg] stroke-blue-600"
-						viewBox="0 0 155 45"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M1.23425 11.4706C2.95888 22.4268 8.74485 52.6144 13.2726 42.4896C16.2053 35.9319 19.7278 29.4742 22.5201 22.9103C26.2267 14.1975 25.6547 22.1938 28.052 27.9103C28.7813 29.6493 34.7482 43.5469 37.0669 36.421C39.6151 28.5898 41.3825 20.9744 47.1616 14.804C51.8581 9.78936 55.4994 19.0694 57.6274 22.4452C63.7019 32.0819 63.3756 29.3529 66.3876 19.3887C67.2051 16.684 74.08 -0.121181 74.4445 0.739637C76.1767 4.83043 77.0939 10.2234 77.8279 14.5769C78.448 18.254 82.5655 42.7245 84.6722 28.9126C85.8721 21.0458 93.565 -1.08979 93.8255 6.86372C93.998 12.1324 91.156 28.7049 97.8623 30.906C101.409 32.07 106.387 27.7962 109.358 26.4984C116.981 23.1681 125.175 20.0126 133.313 18.2148C136.06 17.6079 154.712 17.6977 154.167 12.3122C153.729 7.98598 145.55 -2.07876 143.978 6.12724C142.839 12.072 141.897 17.9113 139.891 23.6523C139.732 24.1086 136.304 33.8258 136.37 33.8296C140.247 34.051 151.87 17.6529 153.037 13.464"
-							strokeWidth="2"
-							strokeLinecap="round"
-						></path>
-					</svg>
-					<div className="[&>svg]:w-64 [&>svg]:h-64 [&>svg]:mx-auto [&>svg]:-mt-28 [&>svg]:hover:rotate-6">
-						<HeadShot />
-					</div>
 				</section>
 
 				{/* <section>
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,21 @@
+import HeadShot from "@/components/headShot";
+
 export default function Footer() {
 	return (
-		<footer className="mt-32 py-12 max-w-prose mx-auto text-center relative">
-			<div className="absolute inset-0 h-24 my-auto bg-fuchsia-200"></div>
-			<div className="z-10 relative transform -rotate-2">
-				<h2 className="font-bold text-3xl mb-4 font-space">Bapak D. Ev</h2>
-				<p className="text-sm px-12 font-jet">
-					Dad with over 10 years of experience as a web developer, specializing in WordPress, Next.js, Astro, and Tailwind CSS. Since 2015, I have been
-					successfully working remotely, embracing the flexibility and freedom it offers.
-				</p>
-			</div>
+		<footer className="mt-32 py-12 max-w-prose mx-auto text-center group">
+			<section className="[&>svg]:w-64 [&>svg]:h-64 [&>svg]:mx-auto group-hover:[&>svg]:rotate-6 transition duration-75 ease-in-out">
+				<HeadShot />
+			</section>
+			<section className="relative -mt-10">
+				<div className="absolute inset-0 h-24 my-auto bg-fuchsia-200"></div>
+				<div className="z-10 relative transform -rotate-2">
+					<h2 className="font-bold text-3xl mb-4 font-space">Bapak D. Ev</h2>
+					<p className="text-sm px-12 font-jet">
+						Dad with over 10 years of experience as a web developer, specializing in WordPress, Next.js, Astro, and Tailwind CSS. Since 2015, I have been
+						successfully working remotely, embracing the flexibility and freedom it offers.
+					</p>
+				</div>
+			</section>
 		</footer>
 	);
 }
